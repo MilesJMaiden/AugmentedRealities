e4c4bb685417805a70d1fbd924e6f970111bfdbe
BattlestationX2\JJD <MilesJMaiden@Gmail.com>
2023-11-19 14:40:22 +0000

    Switched to android build platform + added script to project shadow on ground(layer)

--- MR Game/Assets/Scripts/ProjectOnGround.cs
+++ MR Game/Assets/Scripts/ProjectOnGround.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ProjectOnGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform followTarget;
+    public float raycastDistance = 2f;
+    public LayerMask layerMask;
+    public float spherecastRadius = 0.2f;
+    public float verticalOffset = 0.02f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit raycastHit;
+
+        bool hasHit = Physics.SphereCast(followTarget.position, spherecastRadius, Vector3.down, out raycastHit, raycastDistance, layerMask);
+
+        if(hasHit)
+        {
+            transform.position = new Vector3(followTarget.position.x, raycastHit.point.y + verticalOffset, followTarget.position.z);
+        }
     }
 }
