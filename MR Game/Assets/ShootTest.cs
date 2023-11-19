5def4033f676968ea31bff49f372d21c94daa0b8
BattlestationX2\JJD <MilesJMaiden@Gmail.com>
2023-11-19 15:28:06 +0000

    Fire Projectile script. Build on this to OUT the hit info. physics collision**

---
+++ MR Game/Assets/ShootTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    public GameObject prefab;
+    public float startingVelocity = 12f;
+
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity);
+            Rigidbody projectilRB = projectile.GetComponent<Rigidbody>();
+            projectilRB.velocity = transform.forward * startingVelocity;
+        }
+    }
+}
