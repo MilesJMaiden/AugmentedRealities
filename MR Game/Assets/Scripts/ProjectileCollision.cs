4e03ba953a614038b6b42c45364bff59e8dc814e
BattlestationX2\JJD <MilesJMaiden@Gmail.com>
2023-11-22 13:53:43 +0000

    Script ref for sounds on projectile prefabs + expanded on projectile collision script

--- MR Game/Assets/Scripts/ProjectileCollision.cs
+++ MR Game/Assets/Scripts/ProjectileCollision.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class ProjectileCollision : MonoBehaviour
 {
+    public GameObject particleEffectPrefab; // Particle effect prefab
+    public AudioClip collisionSound; // Sound to play on collision
+    private bool hasCollided = false; // Flag to ensure collision logic only happens once
+
     void OnCollisionEnter(Collision collision)
     {
-        // Get the name of the projectile
-        string projectileName = gameObject.name;
+        if (hasCollided) return; // Prevent multiple collision handling
+        hasCollided = true;
 
-        // Log the projectile name, the object it hit, and the collision position
-        Debug.Log(projectileName + " Projectile has collided with " + collision.gameObject.name + " at position " + collision.contacts[0].point);
+        // Instantiate the particle effect at the collision point
+        if (particleEffectPrefab != null)
+        {
+            Instantiate(particleEffectPrefab, collision.contacts[0].point, Quaternion.identity);
+        }
 
-        // Optional: Destroy the projectile after collision
-        Destroy(gameObject);
+        // Play the collision sound at the collision point
+        if (collisionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(collisionSound, collision.contacts[0].point);
+        }
+
+        // Disable the Mesh Renderer to make the projectile invisible
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null) meshRenderer.enabled = false;
+
+        // Disable the Collider to prevent further collision interactions
+        Collider collider = GetComponent<Collider>();
+        if (collider != null) collider.enabled = false;
+
+        // Make the Rigidbody kinematic to stop it from moving
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null) rb.isKinematic = true;
+
+        // Destroy the projectile GameObject after a delay
+        Destroy(gameObject, collisionSound != null ? collisionSound.length : 0f);
     }
 }
